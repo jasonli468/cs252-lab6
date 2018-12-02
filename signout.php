f2bed2f4249cf2726f21623c522dd77e3eb8768e
Jason Li <jasonli468@gmail.com>
2018-12-01 23:11:27 -0500

    sign out

---
+++ signout.php
@@ -0,0 +1,23 @@
+<?php
+    session_save_path('/home/campus/li2384/www//tmp');
+    session_start();
+    session_unset();
+    session_destroy();
+    header( "refresh:3;url=index.php" );
+?>
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
+    <title>Home - Hungry but Indicisive Boiler</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php include 'header.php';?>
+<div class='container'>
+    You've been successfully signed out. Returning to the home page in 3 seconds...
+</div>
+</body>
